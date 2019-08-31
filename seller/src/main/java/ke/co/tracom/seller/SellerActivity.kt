df5f6dcab9db33b645f13802bac1221ed6f45597
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-31 14:17:42 +0300

    Added library module

---
+++ seller/src/main/java/ke/co/tracom/seller/SellerActivity.kt
@@ -0,0 +1,12 @@
+package ke.co.tracom.seller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller)
+    }
+}
